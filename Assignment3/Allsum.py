08a2297411f0b97c6c49d8b79efd0459ba339273
akshatakamble-14 <157333216+akshatakamble-14@users.noreply.github.com>
2024-02-13 13:37:10 +0530

    Create Allsum.py

---
+++ Assignment3/Allsum.py
@@ -0,0 +1,7 @@
+def sum_list_items(my_list):
+  total_sum = sum(my_list)
+  return total_sum
+
+my_list = [1,2,3,4,5]
+result = sum_list_items(my_list)
+print(f"The sum of the items in the list is : {result}")

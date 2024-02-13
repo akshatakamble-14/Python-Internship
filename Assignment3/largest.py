2acb6567e244b6219db171243292b69666d3c4d1
akshatakamble-14 <157333216+akshatakamble-14@users.noreply.github.com>
2024-02-13 13:46:22 +0530

    Create largest.py

---
+++ Assignment3/largest.py
@@ -0,0 +1,9 @@
+lis = []
+num = int(input("How many elements you want to enter :  ")
+
+for i in ramge(1, num+1):
+  element = int(input("enter exact number:  "))
+  lis.append(element)
+
+print("Original list is :  ", lis)
+print("Largest number is:  ", max(lis))
